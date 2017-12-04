740a169c19ff3602ab9c886ac3c18574b13314e8
Taylor Smith <righteousquesty@gmail.com>
2017-12-04 01:50:06 -0800

    Delete devices, styling and link updates, added images

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module LockBox
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    # set rails to serve assets
+    config.serve_static_assets = true
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
