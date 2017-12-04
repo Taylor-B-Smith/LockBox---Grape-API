a0ad82369c6d7ff7084e126ca59ee30f04ec8c8a
Taylor Smith <righteousquesty@gmail.com>
2017-12-04 02:38:50 -0800

    webpacker removed, production env configured to precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,6 +28,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
+  ##### Seems like Heroku production environment needs this to precompile
   config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
